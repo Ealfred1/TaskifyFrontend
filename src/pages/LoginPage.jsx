7bfff7ec541b76dc9bc5adce686b895e7bd57e76
ealfred1 <alfrederic371@gmail.com>
2024-01-01 02:26:43 +0100

    login successful

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import axios from '../api/axios'
 import LoginImage from '../assets/login.png'
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 
 const LoginPage = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [showPassword, setShowPassword] = useState(false)
-  const [userErr, setUserErr] = useState('')
-  const [pwdErr, setPwdErr] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
+  
+  const LOGIN_URL = '/login/'
   
   const handleUsernameChange = (e) => {
     setUsername(e.target.value)
@@ -24,19 +28,51 @@ const LoginPage = () => {
     setShowPassword(!showPassword)
   }
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    
-    !username && username.trim() === '' ? setUserErr('Username is required') : setUserErr('')
-    
-    !password && password.trim() === '' ? setPwdErr('Username is required') : setPwdErr('')
-    
-    username && password ? console.log(username, password) : ''
-  }
+      try {
+        const response = await axios.post(LOGIN_URL, 
+          JSON.stringify({ username, password }),
+          {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+          }
+        )
+        
+        console.log(response?.data)
+        toast.success("Login Successful !", {
+          position: toast.POSITION.TOP_RIGHT,
+        })
+        setUsername('')
+        setPassword('')
+      
+      } catch (err) {
+        if (!err?.response) {
+          setErrorMsg('No Server Response')
+          console.log(errorMsg)
+          toast.error(errorMsg, {
+            position: toast.POSITION.TOP_RIGHT,
+         })
+        } else if (err.response.status === 400) {
+          setErrorMsg(err.response.data.error)
+          console.log(errorMsg)
+          toast.error(errorMsg, {
+              position: toast.POSITION.TOP_RIGHT,
+           })          
+        } else {
+          setErrorMsg('Login Failed')
+          console.log(errorMsg)
+          toast.error(errorMsg, {
+            position: toast.POSITION.TOP_RIGHT,
+         })
+        }
+      }
+    }
+  
   
   return (
     <div className="flex flex-col justify-center items-center w-full relative h-screen bg-white">
-    
+          <ToastContainer />
     <img src={LoginImage} alt="Login image" className="w-full h-52 object-contain" />
     
       <h1 className="text-gradient text-3xl text-gray-800 mt-9 font-bold">Sign In</h1>
@@ -46,7 +82,7 @@ const LoginPage = () => {
             <input type="text" onChange={handleUsernameChange} value={username} id="username" className="auth-field" required />
             <label htmlFor="username">Username</label>
           </div>
-          {userErr && <span className="error-message">{userErr}</span>}
+
           <div className="input-con">
             <input type={showPassword ? 'text' : 'password'}  id="password" value={password} className="auth-field-pass" onChange={handlePasswordChange} required />
             <label htmlFor="password">Password</label>
@@ -54,7 +90,6 @@ const LoginPage = () => {
               ( <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} className="pass-toggle" onClick={togglePasswordVisibility} /> )
             }
           </div>
-          {pwdErr && <span className="error-message">{pwdErr}</span>}
           <div className="px-2 text-center">
             <button type="submit" className="btn-auth"> Sign In</button>
             <h3 className="mt-2"> Don't have an account? <Link to="/register" className="text-purpleP font-bold"> Sign Up </Link></h3>
