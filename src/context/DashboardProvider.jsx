156c8a5544e3610ea102e25e60b96ba1d9cd85a7
ealfred1 <alfrederic371@gmail.com>
2024-01-21 01:26:22 +0100

    dashboard lil design

--- src/context/DashboardProvider.jsx
+++ src/context/DashboardProvider.jsx
@@ -4,14 +4,14 @@ import axiosPrivate from '../api/axiosPrivate'
 const DashboardContext = createContext({})
 
 export const DashboardProvider = ({ children }) => {
-  const [dashboardData, setDashboardData] = useState({})
+  const [dashboardData, setDashboardData] = useState(localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : {})
   
   const fetchdata = async () => {
     try {
       const response = await axiosPrivate.get('/dashboard/')
-      console.log(response.data)
+      console.log('data', response.data)
       setDashboardData(response.data)
-      
+      localStorage.setItem('data', JSON.stringify(response.data))
     } catch (err) {
       console.log('error', err)
     }
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,19 +1,36 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import useDashboard from '../hooks/useDashboard'
 import axiosPrivate from '../api/axiosPrivate'
 
 const Dashboard = () => {
   const { auth } = useAuth()
-  const data = useAuth()
+  const { dashboardData } = useDashboard()
+  const [greeting, setGreeting] = useState('')
   
   useEffect(() => {
-    console.log(data)
+    const getCurrentTime = () => {
+      const hour = new Date().getHours()
+      if (hour >= 0 && hour < 12) {
+        setGreeting('Good Morning')
+      } else if (hour >= 12 && hour < 18) {
+        setGreeting('Good Afternoon')
+      } else {
+        setGreeting('Good Evening')
+      }
+    }
+
+    getCurrentTime()
   }, [])
-  
+
   return (
-    <div className="bg-blue-300 h-screen w-full flex flex-wrap">
-    <h1 className="text-2xl text-blue"> Hi { auth.user.username } </h1>
+    <div className="h-screen w-[25.8rem] sm:w-full px-1">
+      <div className="flex items-center justify-center">
+        <div className="w-full bg-blue-100 h-80 sm:h-72 p-6 space-y-7 rounded-xl ">
+          <h1 className="text-2xl text-gray-800 font-bold mb-4"> {greeting}, {auth.user.username} </h1>
+          <h1 className="text-xl text-gray-500">You have {dashboardData.pending_tasks} pending tasks </h1>
+        </div>
+      </div>
     </div>
   )
 }
