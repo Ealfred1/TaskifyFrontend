fcbdab08c41cc1618d9c3a1a485c279dc2a54729
Ealfred1 <alfrederic371@gmail.com>
2024-02-16 23:03:26 +0100

    build fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-LOCAL_URL = 'http://localhost:8000/api'
-ACTIVE_URL = 'https://taskify-fl.onrender.com/api'
+const LOCAL_URL = 'http://localhost:8000/api'
+const ACTIVE_URL = 'https://taskify-fl.onrender.com/api'
 
 export default axios.create({
   baseURL: ACTIVE_URL
