dcf11afd6f1e871613d00e2bc3cdacb94e1abd34
Ealfred1 <alfrederic371@gmail.com>
2024-02-20 01:50:41 +0100

    upcoming tasksand celemnder

---
+++ src/components/UpcomingTasks.jsx
@@ -0,0 +1,24 @@
+
+const UpcomingTasks = ({ data }) => {
+	const getUpcomingTasks = () => {
+		const today = new Date()
+
+		return (
+			<div>
+				{data?.recent_tasks && data?.recent_tasks > 0 ? (
+					data.recent_tasks.filter((task) => new Date(task.due_date) >= today)
+					.map((task) => (
+						<div key={task.id}>
+							<h1 className="text-xl sm:text-2xl text-gray-800 font-bold">{task.title}</h1>
+						</div>
+					))
+				) : (
+					<p className="text-xl sm:text-2xl text-gray-800 font-bold">No upcoming tasks</p>
+				)}
+			</div>
+		)
+	}
+	return getUpcomingTasks()
+}
+
+export default UpcomingTasks
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -3,6 +3,7 @@ import { Calendar } from 'primereact/calendar'
 import useAuth from '../hooks/useAuth'
 import useDashboard from '../hooks/useDashboard'
 import axiosPrivate from '../api/axiosPrivate'
+import UpcomingTasks from '../components/UpcomingTasks'
 
 const Dashboard = () => {
   const { auth } = useAuth()
@@ -39,18 +40,22 @@ const Dashboard = () => {
     <>
 
     <div className="px-1 w-full overflow-hidden pr-4">
-      <div className="flex flex-col lg:flex-row items-center justify-between sm:space-x-5 space-y-5">
-        <div className={`w-full sm:w-[60%] z-10 ${ greeting === 'Good Morning' && 'Good Afternoon' ? 'bg' :
+      <div className="flex flex-col lg:flex-row w-[100%] items-center justify-between sm:space-x-2 space-y-5">
+        <div className={`w-full lg:w-[60%] z-10 ${ greeting === 'Good Morning' && 'Good Afternoon' ? 'bg' :
         'bg2' } h-80 sm:h-96 rounded-xl`}>
-          <div className="bg-[rgba(255,255,255,0.12)] w-full h-full p-4 pt-6 space-y-7
+          <div className="bg-[rgba(255,255,255,0.2)] w-full h-full p-4 pt-6 space-y-7
           rounded-xl text-left">
             <h1 className="text-xl sm:text-2xl text-gray-800 font-bold mb-4"> {greeting}, {auth.user.username} </h1>
-            <h1 className="text-lg sm:text-xl text-gray-700">You have {dashboardData.pending_tasks} pending tasks </h1>
+            <h1 className="text-lg sm:text-xl text-gray-900">You have {dashboardData.pending_tasks} pending tasks </h1>
+
+            <div className="">
+              <UpcomingTasks data={dashboardData} />
+            </div>
           </div>
         </div>
 
-         <div className={`sm:w-[40%] w-full h-80 sm:h-96 rounded-xl border border-gray-400 mr-2 md:mr-4 lg:border-none`}>
-          <Calendar className="w-full" value={date} onChange={(e) => setDate(e.value)} inline />
+         <div className={`lg:w-[40%] h-80 sm:h-96 rounded-2xl border border-gray-400 mr-2 md:mr-4 lg:border-none shadow-lg shadow-gray-300`}>
+          <Calendar className="w-full sm:w-auto" value={date} onChange={(e) => setDate(e.value)} inline />
         </div>
       </div>
     </div>
