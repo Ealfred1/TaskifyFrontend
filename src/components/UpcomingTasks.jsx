b502bb4d70666c676e8195ca1649b8f0a35d7722
Ealfred1 <alfrederic371@gmail.com>
2024-03-01 23:57:33 +0100

    changes

--- src/components/UpcomingTasks.jsx
+++ src/components/UpcomingTasks.jsx
@@ -1,26 +1,30 @@
+import { useState } from 'react'
 
 const UpcomingTasks = ({ data }) => {
-	const getUpcomingTasks = () => {
-		const today = new Date()
+  const getUpcomingTasks = () => {
+    const today = new Date()
 
-		return (
-			<div>
-				{data?.recent_tasks && data?.recent_tasks.length > 1 ? (
-					<h1 className="text-xl sm:text-2xl text-gray-800 font-bold">Upcoming tasks</h1>
-					data.recent_tasks.filter((task) => new Date(task.due_date) >= today)
-					.map((task) => (
-						<div key={task.id}>
-							
-							<h1 className="text-xl sm:text-2xl text-gray-800 font-bold">{task.title}</h1>
-						</div>
-					))
-				) : (
-					<p className="text-xl sm:text-2xl text-gray-800 font-bold">No upcoming tasks</p>
-				)}
-			</div>
-		)
-	}
-	return getUpcomingTasks()
+    return (
+      <div>
+        {data?.recent_tasks && data?.recent_tasks.length > 1 && data.recent_tasks.filter((task) => new Date(task.due_date) >= today) > 0 ?  (
+          <div>
+            {data.recent_tasks
+              .filter((task) => new Date(task.due_date) >= today)
+              .map((task) => (
+                <div key={task.id}>
+                <h1 className="text-xl sm:text-2xl text-gray-800 font-bold">Upcoming tasks</h1>
+                  <h1 className="text-xl sm:text-2xl text-gray-800 font-bold">{task.title}</h1>
+                </div>
+              ))}
+          </div>
+        ) : (
+          <p className="text-xl sm:text-2xl text-gray-800 font-bold">No upcoming tasks</p>
+        )}
+      </div>
+    )
+  }
+
+  return getUpcomingTasks()
 }
 
 export default UpcomingTasks
