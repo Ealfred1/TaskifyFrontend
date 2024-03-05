409383eb0481d88a3212a54f28916db9e66a359b
Ealfred1 <alfrederic371@gmail.com>
2024-03-05 23:12:09 +0100

    changes on api folder

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -41,8 +41,7 @@ const Dashboard = () => {
 
     <div className="px-1 w-full overflow-hidden pr-4">
       <div className="flex flex-col lg:flex-row w-[100%] items-center justify-between sm:space-x-2 space-y-5">
-        <div className={`w-full lg:w-[40%] z-10 ${ greeting === 'Good Morning' || 'Good Afternoon' ? 'bg' :
-        'bg2' } h-80 sm:h-96 rounded-xl`}>
+        <div className={`w-full lg:w-[40%] z-10 ${greeting === 'Good Morning' || greeting === 'Good Afternoon' ? 'bg' : 'bg2' } h-80 sm:h-96 rounded-xl`}>
           <div className="bg-[rgba(255,255,255,0.2)] w-full h-full p-4 pt-6 space-y-7
           rounded-xl text-left">
             <h1 className="text-xl sm:text-2xl text-gray-800 font-bold mb-4"> {greeting}, {auth.user.username} </h1>
