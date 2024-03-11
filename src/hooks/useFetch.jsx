fa9a3f79dc50b93a35d171851ec7497d63cdbead
Ealfred1 <alfrederic371@gmail.com>
2024-03-11 01:51:04 +0100

    changes

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import axiosPrivate from '../api/axiosPrivate'
+
+const useFetch = (url) => {
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  const fetchdata = async () => {
+    try {
+      const response = await axiosPrivate.get(url);
+      console.log('data', response.data);
+      // Assuming you have a state setter function for dashboardData
+      setData(response.data);
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        console.log(err.response.data.error);
+        setError(err.response)
+        // Assuming you have a function to handle the 401 error and retry the request
+        // fetchdata();
+      } else {
+        console.log('Request Failed:', err.message)
+        setError(err.message)
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  
+  useEffect(() => {
+    fetchdata();
+  }, [loading])
+  
+  return { data, loading, error}
+}
+
+export default useFetch
--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,37 +1,13 @@
-import { useState, useEffect } from 'react';
-import axiosPrivate from '../api/axiosPrivate';
+import { useState } from 'react';
 import CategoryList from '../components/CategoryList'
+import useFetch from '../hooks/useFetch'
 
 const CategoryPage = () => {
-	const [categoryData, setCategoryData] = useState({})
-  const [loading, setLoading] = useState(true);
-
-  const fetchdata = async () => {
-    try {
-      const response = await axiosPrivate.get('/categories/');
-      console.log('CategoryData', response.data);
-      // Assuming you have a state setter function for dashboardData
-      setCategoryData(response.data);
-    } catch (err) {
-      if (err.response && err.response.status === 401) {
-        console.log(err.response.data.error);
-        // Assuming you have a function to handle the 401 error and retry the request
-        // fetchdata();
-      } else {
-        console.log('Request Failed:', err.message);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { data: categoryData, loading, error, } = useFetch('/categories/')
 
   const handleCategoryCreation = (e) => {
     console.log(e.target.value)
   }
-
-  useEffect(() => {
-    fetchdata();
-  }, [loading]);
 
   return (
 		<div className='mx-3'>
