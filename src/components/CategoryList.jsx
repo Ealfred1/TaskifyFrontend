e58cd80fae7b78cf66c61c645de357c51b98e39d
Ealfred1 <alfrederic371@gmail.com>
2024-03-30 12:46:59 +0100

    changes

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import CategoryPopup from './CategoryPopup'
+import useTasks from '../hooks/useTasks'
 
 const CategoryList = ({ categoryData, loading, className }) => {
 	const navigate = useNavigate()
 	const [openMenu, setOpenMenu] = useState(false)
 	const [menuId, setMenuId] = useState(null)
+	const [categoryMessage, setCategoryMessage] = useState('')
+	const { toggle, handleToggle } = useTasks()
 
 	const handleCategoryClick = (id) => {
 		navigate(`/category/${id}`)
@@ -14,16 +18,26 @@ const CategoryList = ({ categoryData, loading, className }) => {
 		setOpenMenu(!openMenu)
 		setMenuId(id)
 	}
+
+	const handleCategoryDelete = (id) => {
+		setMenuId(id)
+		let message = "You about to delete a task category, note that all the task in this category will be deleted as well!"
+		setCategoryMessage(message)
+		handleToggle()
+	}
 	return (
 		<>
 			{ loading && <div className="flex items-center justify-center text-9xl dark:text-gray-300 w-full h-full"><h1 className="text-gray-400 text-center"><i className="pi pi-spinner pi-spin"></i></h1></div> }
+			<div className="">
+					{ categoryMessage.length > 0 && menuId === menuId && (<CategoryPopup message={categoryMessage} categoryId={menuId} toggle={toggle} handleToggle={handleToggle} />) }
+				</div>
 			{ categoryData.length > 0 && categoryData.map((data) => (
 				<div className={`${className} bg-gradient-to-r from-purpleP to-purple-400 dark:to-purple-500 relative`} key={data.id}>
 					<div className="bg-red-500 relative">
-						<button className="w-[2rem] h-[2rem] flex items-center justify-center rounded-full hover:bg-purpleP bg-opacity-100 z-[1] top-0 absolute right-0 text-right text-white" onClick={() => handleMenuClick(data.id)}><span className="pi pi-ellipsis-v"></span></button>
+						<button className="w-[2rem] h-[2rem] flex items-center justify-center rounded-full hover:bg-purpleP bg-opacity-100 top-0 absolute right-0 text-right text-white" onClick={() => handleMenuClick(data.id)}><span className="pi pi-ellipsis-v"></span></button>
 						<div className={`w-[10rem] h-[5rem] py-2 duration-500 absolute top-8 right-0 bg-[rgba(0,0,0,0.4)] backdrop-blur-2xl rounded-md flex flex-col ${openMenu && menuId === data.id ? 'scale-[1]' : 'scale-[0]'}`}>
 							<a className="text-center w-full pt-1 rounded-t-md text-gray-300 h-[2rem] hover:bg-[rgba(255,255,255,0.1)]">Edit</a>
-							<a className="text-center w-full pt-1 rounded-b-md h-[2rem] text-gray-300 hover:bg-[rgba(255,255,255,0.1)]">Delete</a>
+							<a className="text-center w-full pt-1 rounded-b-md h-[2rem] text-gray-300 hover:bg-[rgba(255,255,255,0.1)]" onClick={() => handleCategoryDelete(data.id)}>Delete</a>
 						</div>
 					</div>
 					<div className="flex items-center justify-center h-full" onClick={() => handleCategoryClick(data.id)}>
@@ -32,6 +46,7 @@ const CategoryList = ({ categoryData, loading, className }) => {
 					<div className="text-right text-white" onClick={() => handleCategoryClick(data.id)}>
 						<p className="text-lg"><i className="bx bx-task"></i>{data.task_count}</p>						
 					</div>
+
 				</div>
 				)) }
 			{ categoryData.length <= 0 && (<div className="text-xl text-gray-500">Oops, you don't have any categories yet!!</div>) }
---
+++ src/components/CategoryPopup.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const CategoryPopup = ({ message, categoryId, toggle, handleToggle }) => {
+
+	const handleSubmit = (e) => {
+		alert('done')
+	}
+	
+	return (
+		<>
+			<div className={`absolute inset-0 z-[1] flex items-center justify-center menu ${toggle ? 'toggle' : '' }`}>
+				<form method="post" onSubmit={handleSubmit} className={`w-[95%] md:w-[60%] h-[18rem] lg:w-[40%] bg-slate-50 dark:bg-slate-900 border border-gray-200 dark:border-none dark:outline dark:outline-[1px] dark:outline-gray-500 h-[70%] flex flex-col fixed z-[1] rounded-lg shadow-2xl menu ${toggle ? 'toggle' : '' }`}>
+				<div className="w-full rounded-t-lg h-14 mb-4 font-bold bg-red-500 flex items-center justify-center text-white">
+					Delete Category
+				</div>
+				<label className="task-label text-lg text-gray-300 px-2">{ message }</label>
+
+				<div className="flex justify-between absolute bottom-0 px-2 w-full gap-4 mt-4 bg-slate-50 dark:bg-slate-900 rounded-lg py-2">
+			          <a onClick={handleToggle} className="bg-none border border-gray-500 btn2 text-gray-500">Go back</a>
+			          <button type="submit" className="text-center w-1/2 h-14 text-lg rounded-lg text-white bg-red-500 hover:scale-[1.033]">Delete</button>
+			    </div>
+				</form>
+			</div>
+		</>
+	)
+}
+
+export default CategoryPopup
