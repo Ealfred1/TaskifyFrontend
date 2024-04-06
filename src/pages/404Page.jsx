0f3aa134ff2376f61683d103b8642a8c4925c34c
Ealfred1 <alfrederic371@gmail.com>
2024-04-06 01:25:20 +0100

    random

--- src/pages/404Page.jsx
+++ src/pages/404Page.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
 		<div className="w-full flex items-center justify-center flex-col px-2">
         		<img src={Image} alt="In development" className="" />
         		<h1 className="dark:text-white text-3xl">Oops!!! Seems you're lost? </h1>
-        		<Link to='/' className="w-full h-16 text-white text-center bg-purpleP my-3 flex items-center justify-center text-2xl rounded-xl">Go Back</Link>
+        		<Link to='/' className="w-full max-w-3xl h-16 text-white text-center bg-purpleP my-3 flex items-center justify-center text-2xl rounded-xl">Go Back</Link>
       	</div>
 	)
 }
